9c9dcb2f7987684a93a0413d59b18561b95d7eae
hassoun21 <potox2017@gmail.com>
2026-09-12 12:40:01 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,9 +5,17 @@ const cheerio = require('cheerio');
 
 const app = express();
 
+// إعطاء صلاحيات الاتصال للواجهة
 app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 app.use(express.json());
-app.use(express.static(__dirname));
+app.use(express.static('public'));
+
+const PORT = 3000;
 
 const PORT = 3000;
 const TARGET_URL = 'https://animezid.cam';
